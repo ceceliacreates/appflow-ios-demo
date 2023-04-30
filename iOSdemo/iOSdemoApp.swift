ca0fa8e6b283096f35befbc2495c0724226fa84e
ceceliacreates <ceceliacreates@gmail.com>
2023-04-30 08:18:01 -0500

    Initial Commit

---
+++ iOSdemo/iOSdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSdemoApp.swift
+//  iOSdemo
+//
+//  Created by Cecelia Martinez on 4/30/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
